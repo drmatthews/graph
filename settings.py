1c04a65c1e53bfd45eae8847755053d24af6a1ea
Daniel Matthews <dr.dan.matthews@gmail.com>
2015-09-11 20:45:53 +0100

    added settings.py

---
+++ settings.py
@@ -0,0 +1,7 @@
+import json
+CUSTOM_SETTINGS_MAPPINGS = {
+    "omero.web.plot.plotly_username": 
+    						["PLOTLY_USERNAME", "", str, None],
+    "omero.web.plot.plotly_apikey": 
+    						["PLOTLY_APIKEY", "", str, None]
+}
